c06ac66774ca332ff495524af86bb3a26dc06082
nacerillo <nicholascerillo@gmail.com>
2021-09-17 13:20:17 -0400

    adding modal and button components that will allow user to add a new task and assign a technican to it

--- it-logger/src/App.js
+++ it-logger/src/App.js
@@ -4,6 +4,8 @@ import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import Logs from './components/logs/Logs';
 import SearchBar from './components/layout/SearchBar';
+import AddButton from './components/layout/AddButton';
+import AddLogModal from './components/layout/AddLogModal';
 import "./app.css"
 const App = () => {
    useEffect(() => {
@@ -14,6 +16,8 @@ const App = () => {
     <div className="App">
       <SearchBar/>
       <div className = "contianer" id = "app-container">
+        <AddButton/>
+        <AddLogModal/>
         <Logs/>
       </div>
     </div>
