2d8fc80b987a43d528a89ac0db48582755e28ced
nacerillo <nicholascerillo@gmail.com>
2021-09-21 17:15:17 -0400

    redux implementing for getting logs

--- it-logger/src/components/logs/Logs.js
+++ it-logger/src/components/logs/Logs.js
@@ -1,26 +1,33 @@
 import React, {useState, useEffect} from 'react'
 import LogItem from './LogItem';
 import Preloader from '../layout/Preloader';
-const Logs = () => {
-    const [logs,setLogs] = useState([]);
-    const [loading, setLoading] = useState(false);
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import { getLogs } from '../../actions/logActions';
+const Logs = ({log: {logs, loading}, getLogs}) => {
+
+    //this is now coming from app level state thanks to redux
+   // const [logs,setLogs] = useState([]);
+    //const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         getLogs()
         //eslint-disable-next-line
     },[]);
-    const getLogs = async () =>{
+
+    /*const getLogs = async () =>{
         setLoading(true);
         const res = await fetch('/logs');
         const data = await res.json();
         
         setLogs(data);
         setLoading(false);
-    }
+    }*/
 
-    if (loading){
+    if (loading || logs === null){
         return <Preloader/>
     }
+
     return (
         <ul className = "collection with-header">
             <li className = "collection-header">
@@ -30,7 +37,15 @@ const Logs = () => {
                 logs.map( log => <LogItem log = {log} key = {log.id}/>)
             )}
         </ul>
-    )
-}
+    );
+};
 
-export default Logs
+Logs.propTypes = {
+    log: PropTypes.object.isRequired,
+}
+const mapStateToProps = state => ({
+    log: state.log //this pertains to log = logReducer from our reducer
+})
+
+//connects redux to our Logs component, add function we want to use from reducer as a second parameter
+export default connect(mapStateToProps, {getLogs})(Logs);
